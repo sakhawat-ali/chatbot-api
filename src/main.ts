b1bc5b1429d462d2d82f1f61d86929af9a1e75d6
sakhawat-ali <m.sakhawat.ali@gmail.com>
2025-08-09 01:58:49 +0500

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const openApiConfig = new DocumentBuilder()
+    .setTitle('Chatbot API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, openApiConfig);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
